9d26b99b057b1b51bcbbd2670c7e84471608ac3d
Delfina <delfinapazzavalia@gmail.com>
2023-07-28 12:26:31 -0300

    edits

---
+++ src/component/seccionesEcommerce/Pruebas.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from "react-bootstrap/Button";
+
+const Pruebas = () => {
+  return (
+    <div>
+        
+        <Button variant="primary">buy</Button>
+            <Button variant="primary">ok</Button>
+    </div>
+  )
+}
+
+export default Pruebas
